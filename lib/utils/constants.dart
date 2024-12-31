219728c707c61083a9a041cd365bd85e35d744ed
joaquingalang <galangjoaquin@gmail.com>
2024-12-31 22:00:06 +0800

    Added constants.dart file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Color
+const Color kBackgroundColor = Color(0xFFFFFDF9);
+const Color kDeepPurpleColor = Color(0xFF321F47);
+const Color kTealColor = Color(0xFF53C5CF);
+const Color kOrangeColor = Color(0xFFFDC04F);
+const Color kYellowColor = Color(0xFFFAE741);
+const Color kRedColor = Color(0xFFDE6D78);
+
+// Text Style
+const TextStyle kMediumTextStyle = TextStyle(
+  fontFamily: 'Axis',
+  color: Colors.black,
+  fontSize: 20,
+);
